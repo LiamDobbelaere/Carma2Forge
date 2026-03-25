c24b5fd7da6174f904301d344e5c48dc6c47c1db
Liam Dobbelaere <liam.dobbelaere@diekeure.be>
2026-03-25 21:09:19 +0100

    ACT parsing in Unity + render scene

---
+++ Carma2Forge3DEditor/Assets/Scripts/Modules/ActModule.cs
@@ -0,0 +1,239 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+public class ActFileActor {
+  public ActorType type;
+  public RenderStyle renderStyle;
+  public string identifier;
+  public string model;
+  public string material;
+  public Matrix3D transform;
+  public MeshExtents? bounds;
+  public ActLight light;
+  public ActCamera camera;
+  public ActFileActor parent;
+  public List<ActFileActor> children = new List<ActFileActor>();
+}
+
+public class ActFile {
+  public List<ActFileActor> roots = new List<ActFileActor>();
+}
+
+public enum ActCameraType {
+}
+
+public class ActCamera {
+  public ActCameraType type;
+  public ushort fov;
+  public float hitherZ;
+  public float yonZ;
+  public float aspect;
+}
+
+public enum ActLightType {
+}
+
+public class ActLight {
+  public ActLightType type;
+  public byte r, g, b;
+  public Vector3 attenuation;
+  public ushort coneInner;
+  public ushort coneOuter;
+}
+
+public enum ActChunkType {
+  Actor = 35,
+  Model = 36,
+  Transform = 37,
+  Material = 38,
+  Light = 39,
+  CameraOld = 40,
+  Bounds = 41,
+  AddChild = 42,
+  Matrix = 43,
+  BoundingBox = 50,
+  LightOld = 51,
+  Camera = 52,
+  EOF = 0
+}
+
+public enum ActorType {
+  BR_ACTOR_NONE,
+  BR_ACTOR_MODEL,
+  BR_ACTOR_LIGHT,
+  BR_ACTOR_CAMERA,
+  BR_ACTOR_BOUNDS,
+  BR_ACTOR_BOUNDS_CORRECT,
+  BR_ACTOR_CLIP_PLANE
+}
+
+public enum RenderStyle {
+  BR_RSTYLE_DEFAULT,
+  BR_RSTYLE_NONE,
+  BR_RSTYLE_POINTS,
+  BR_RSTYLE_EDGES,
+  BR_RSTYLE_FACES = 0x4,
+  BR_RSTYLE_BOUNDING_POINTS,
+  BR_RSTYLE_BOUNDING_EDGES,
+  BR_RSTYLE_BOUNDING_FACES
+}
+
+public class Matrix3D {
+  public float M11, M12, M13;
+  public float M21, M22, M23;
+  public float M31, M32, M33;
+  public float M41, M42, M43;
+
+  public static Matrix3D Identity => new Matrix3D(1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0);
+
+  public Matrix3D(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33, float m41, float m42, float m43) {
+    M11 = m11; M12 = m12; M13 = m13;
+    M21 = m21; M22 = m22; M23 = m23;
+    M31 = m31; M32 = m32; M33 = m33;
+    M41 = m41; M42 = m42; M43 = m43;
+  }
+}
+
+public struct MeshExtents {
+  public Vector3 Min;
+  public Vector3 Max;
+
+  public MeshExtents(Vector3 min, Vector3 max) {
+    Min = min;
+    Max = max;
+  }
+}
+
+public class ActModule {
+  private static UInt32[] actHeader = { 0x12, 0x8, 0x1, 0x2 };
+
+  public ActFile LoadAct(TwtFileEntry twtFileEntry) {
+    MemoryStream stream = new MemoryStream(twtFileEntry.data);
+    ActFile act = new ActFile();
+    Stack<object> stack = new Stack<object>();
+
+    using (BinaryReader br = new BinaryReader(stream)) {
+      ValidateActHeader(br);
+
+      while (br.BaseStream.Position < br.BaseStream.Length) {
+        ActChunkType chunkType = (ActChunkType)br.ReadUInt32BE();
+        int length = (int)br.ReadUInt32BE();
+
+        switch (chunkType) {
+          case ActChunkType.Actor: {
+            ActFileActor actor = new ActFileActor();
+            actor.type = (ActorType)br.ReadByte();
+            actor.renderStyle = (RenderStyle)br.ReadByte();
+            actor.identifier = br.ReadNullTerminatedString();
+            if (actor.identifier == string.Empty) {
+              actor.identifier = "NO_IDENTIFIER";
+            }
+            actor.transform = Matrix3D.Identity;
+            stack.Push(actor);
+            break;
+          }
+
+          case ActChunkType.Model:
+            ((ActFileActor)stack.Peek()).model = br.ReadNullTerminatedString();
+            break;
+
+          case ActChunkType.Material:
+            ((ActFileActor)stack.Peek()).material = br.ReadNullTerminatedString();
+            break;
+
+          case ActChunkType.Matrix:
+            stack.Push(new Matrix3D(
+              br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE(),
+              br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE(),
+              br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE()
+            ));
+            break;
+
+          case ActChunkType.Transform: {
+            Matrix3D transform = (Matrix3D)stack.Pop();
+            ((ActFileActor)stack.Peek()).transform = transform;
+            break;
+          }
+
+          case ActChunkType.BoundingBox:
+            stack.Push(new MeshExtents(
+              new Vector3(br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE()),
+              new Vector3(br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE())
+            ));
+            break;
+
+          case ActChunkType.Bounds: {
+            MeshExtents bounds = (MeshExtents)stack.Pop();
+            ((ActFileActor)stack.Peek()).bounds = bounds;
+            break;
+          }
+
+          case ActChunkType.LightOld:
+            stack.Push(new ActLight {
+              type = (ActLightType)br.ReadByte(),
+              r = br.ReadByte(),
+              g = br.ReadByte(),
+              b = br.ReadByte(),
+              attenuation = new Vector3(br.ReadSingleBE(), br.ReadSingleBE(), br.ReadSingleBE()),
+              coneInner = br.ReadUInt16BE(),
+              coneOuter = br.ReadUInt16BE()
+            });
+            break;
+
+          case ActChunkType.Light: {
+            ActLight light = (ActLight)stack.Pop();
+            ((ActFileActor)stack.Peek()).light = light;
+            break;
+          }
+
+          case ActChunkType.Camera:
+            stack.Push(new ActCamera {
+              type = (ActCameraType)br.ReadByte(),
+              fov = br.ReadUInt16BE(),
+              hitherZ = br.ReadSingleBE(),
+              yonZ = br.ReadSingleBE(),
+              aspect = br.ReadSingleBE()
+            });
+            break;
+
+          case ActChunkType.CameraOld: {
+            ActCamera camera = (ActCamera)stack.Pop();
+            ((ActFileActor)stack.Peek()).camera = camera;
+            break;
+          }
+
+          case ActChunkType.AddChild: {
+            ActFileActor child = (ActFileActor)stack.Pop();
+            ActFileActor parent = (ActFileActor)stack.Peek();
+            child.parent = parent;
+            parent.children.Add(child);
+            break;
+          }
+
+          case ActChunkType.EOF:
+            if (stack.Count > 0 && stack.Peek() is ActFileActor root) {
+              stack.Pop();
+              act.roots.Add(root);
+            }
+            break;
+
+          default:
+            throw new Exception($"Unsupported chunk type {chunkType} in ACT file");
+        }
+      }
+    }
+
+    return act;
+  }
+
+  private void ValidateActHeader(BinaryReader reader) {
+    for (int i = 0; i < actHeader.Length; i++) {
+      uint value = reader.ReadUInt32BE();
+      if (actHeader[i] != value) {
+        throw new Exception($"Unexpected value {value} where {actHeader[i]} was expected");
+      }
+    }
+  }
+}
--- Carma2Forge3DEditor/Assets/Scripts/RaceLoader.cs
+++ Carma2Forge3DEditor/Assets/Scripts/RaceLoader.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,8 +6,8 @@ public class RaceLoader : MonoBehaviour
 {
     public Material defaultMaterial;
     private Dictionary<string, Material> materialCache = new Dictionary<string, Material>();
+    private Dictionary<string, DatMesh> meshLookup = new Dictionary<string, DatMesh>();
 
-    // Start is called before the first frame update
     void Start()
     {
         TwtModule twtModule = new TwtModule();
@@ -17,111 +16,143 @@ public class RaceLoader : MonoBehaviour
         TwtFile texturesTwtFile = twtModule.LoadTwt("D:\\SteamLibrary\\steamapps\\common\\Carmageddon2\\data\\RACES\\NEWC.TWT");
         DatFile datFile = new DatModule().LoadDat(twtFile.GetFile("newcity1.dat"));
         MatFile matFile = new MatModule().LoadMat(twtFile.GetFile("newcity1.mat"));
+        ActFile actFile = new ActModule().LoadAct(twtFile.GetFile("newcity1.act"));
         Dictionary<string, MatFileMaterial> materialsByName = matFile.GetMaterialsByName();
-
         PixiesFile pixiesFile = pixiesModule.LoadPixies(texturesTwtFile.GetFile("PIXIES.P16"));
 
+        // Build mesh lookup by name
         foreach (DatMesh mesh in datFile.meshes)
         {
-            // create a new unity gameobject as child
-            GameObject meshObj = new GameObject(mesh.name);
+            meshLookup[mesh.name] = mesh;
+        }
 
-            // add a mesh filter and renderer
-            MeshFilter meshFilter = meshObj.AddComponent<MeshFilter>();
-            MeshRenderer meshRenderer = meshObj.AddComponent<MeshRenderer>();
-
-            // Group faces by materialId for submeshes
-            // materialId is 1-based in BRender (0 = no material assigned)
-            var facesByMaterial = mesh.faces
-                .Where(f => f.materialId > 0)
-                .GroupBy(f => f.materialId)
-                .OrderBy(g => g.Key)
-                .ToList();
-
-            // convert dat mesh to unity mesh
-            Mesh unityMesh = new Mesh
-            {
-                vertices = System.Array.ConvertAll(mesh.vertices, v => new Vector3(-v.X, v.Y, v.Z)),
-                uv = System.Array.ConvertAll(mesh.uvs, uv => new Vector2(uv.X, 1f - uv.Y)),
-                subMeshCount = facesByMaterial.Count
-            };
-
-            for (int i = 0; i < facesByMaterial.Count; i++)
-            {
-                int[] triangles = facesByMaterial[i]
-                    .SelectMany(f => new int[] { f.v1, f.v3, f.v2 })
-                    .ToArray();
-                unityMesh.SetTriangles(triangles, i);
-            }
-
-            unityMesh.RecalculateNormals();
-            unityMesh.RecalculateBounds();
-
-            // Assign a material per submesh
-            Material[] materials = new Material[facesByMaterial.Count];
-            for (int i = 0; i < facesByMaterial.Count; i++)
-            {
-                int matIndex = facesByMaterial[i].Key - 1; // materialId is 1-based
-                if (mesh.materials != null && matIndex >= 0 && matIndex < mesh.materials.Length)
-                {
-                    string matName = mesh.materials[matIndex];
-                    MatFileMaterial c2mat = materialsByName.ContainsKey(matName) ? materialsByName[matName] : null;
-
-                    Material mat;
-                    if (materialCache.ContainsKey(matName))
-                    {
-                        mat = materialCache[matName];
-                    }
-                    else
-                    {
-                        mat = new Material(defaultMaterial);
-                        mat.color = c2mat != null ? new Color32(c2mat.diffuseColor[0], c2mat.diffuseColor[1], c2mat.diffuseColor[2], c2mat.diffuseColor[3]) : Color.white;
-                        if (c2mat != null && c2mat.flags.HasFlag(MatFileMaterial.Settings.Two_Sided))
-                        {
-                            mat.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
-                        }
-
-                        if (mat.color.a < 1f)
-                        {
-                            // set material to translucent rendering mode
-                            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                            mat.SetInt("_ZWrite", 0);
-                            mat.DisableKeyword("_ALPHATEST_ON");
-                            mat.EnableKeyword("_ALPHABLEND_ON");
-                            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                            mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
-                        }
-
-
-                        string texture = c2mat != null ? c2mat.texture : null;
-
-                        mat.SetTexture("_MainTex", pixiesFile.GetFile(texture)?.bitmap);                        
-                        materialCache[matName] = mat;
-                    }
-                    //mat.color = new Color((float)rand.NextDouble(), (float)rand.NextDouble(), (float)rand.NextDouble());
-
-                    /*foreach (PixiesFileEntry entry in pixiesFile.entries)
-                    {
-                        Debug.Log($"Pixies file entry: {entry.filename}");
-                        Debug.Log($"Looking for {matName.ToLower()} in pixies file entries");
-                    }*/
-
-                    materials[i] = mat;
-                }
-                else
-                {
-                    materials[i] = defaultMaterial;
-                }
-            }
-            meshFilter.mesh = unityMesh;
-            meshRenderer.materials = materials;
+        // Build scene from ACT hierarchy
+        foreach (ActFileActor root in actFile.roots)
+        {
+            BuildActorHierarchy(root, transform, materialsByName, pixiesFile);
         }
     }
 
-    // Update is called once per frame
+    private void BuildActorHierarchy(ActFileActor actor, Transform parentTransform, Dictionary<string, MatFileMaterial> materialsByName, PixiesFile pixiesFile)
+    {
+        GameObject go = new GameObject(actor.identifier);
+        go.transform.SetParent(parentTransform, false);
+
+        // Apply the actor's 3x4 transform matrix
+        ApplyMatrix3D(go.transform, actor.transform);
+
+        // If this actor references a model, attach the mesh
+        if (actor.model != null && meshLookup.ContainsKey(actor.model))
+        {
+            AttachMesh(go, meshLookup[actor.model], materialsByName, pixiesFile);
+        }
+
+        // Recurse into children
+        foreach (ActFileActor child in actor.children)
+        {
+            BuildActorHierarchy(child, go.transform, materialsByName, pixiesFile);
+        }
+    }
+
+    private void ApplyMatrix3D(Transform t, Matrix3D m)
+    {
+        if (m == null) return;
+
+        // BRender (right-handed, row-vector) → Unity (left-handed, column-vector)
+        // M_unity = S * M_br^T * S, where S = diag(-1,1,1,1) flips X axis
+        Matrix4x4 mat = new Matrix4x4(
+            new Vector4( m.M11, -m.M12, -m.M13, 0),
+            new Vector4(-m.M21,  m.M22,  m.M23, 0),
+            new Vector4(-m.M31,  m.M32,  m.M33, 0),
+            new Vector4(-m.M41,  m.M42,  m.M43, 1)
+        );
+
+        t.localPosition = mat.GetColumn(3);
+        t.localRotation = mat.rotation;
+        t.localScale = new Vector3(mat.GetColumn(0).magnitude, mat.GetColumn(1).magnitude, mat.GetColumn(2).magnitude);
+    }
+
+    private void AttachMesh(GameObject go, DatMesh datMesh, Dictionary<string, MatFileMaterial> materialsByName, PixiesFile pixiesFile)
+    {
+        MeshFilter meshFilter = go.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = go.AddComponent<MeshRenderer>();
+
+        var facesByMaterial = datMesh.faces
+            .Where(f => f.materialId > 0)
+            .GroupBy(f => f.materialId)
+            .OrderBy(g => g.Key)
+            .ToList();
+
+        Mesh unityMesh = new Mesh
+        {
+            vertices = System.Array.ConvertAll(datMesh.vertices, v => new Vector3(-v.X, v.Y, v.Z)),
+            uv = System.Array.ConvertAll(datMesh.uvs, uv => new Vector2(uv.X, 1f - uv.Y)),
+            subMeshCount = facesByMaterial.Count
+        };
+
+        for (int i = 0; i < facesByMaterial.Count; i++)
+        {
+            int[] triangles = facesByMaterial[i]
+                .SelectMany(f => new int[] { f.v1, f.v3, f.v2 })
+                .ToArray();
+            unityMesh.SetTriangles(triangles, i);
+        }
+
+        unityMesh.RecalculateNormals();
+        unityMesh.RecalculateBounds();
+
+        Material[] materials = new Material[facesByMaterial.Count];
+        for (int i = 0; i < facesByMaterial.Count; i++)
+        {
+            int matIndex = facesByMaterial[i].Key - 1;
+            if (datMesh.materials != null && matIndex >= 0 && matIndex < datMesh.materials.Length)
+            {
+                string matName = datMesh.materials[matIndex];
+                materials[i] = GetOrCreateMaterial(matName, materialsByName, pixiesFile);
+            }
+            else
+            {
+                materials[i] = defaultMaterial;
+            }
+        }
+
+        meshFilter.mesh = unityMesh;
+        meshRenderer.materials = materials;
+    }
+
+    private Material GetOrCreateMaterial(string matName, Dictionary<string, MatFileMaterial> materialsByName, PixiesFile pixiesFile)
+    {
+        if (materialCache.ContainsKey(matName))
+            return materialCache[matName];
+
+        MatFileMaterial c2mat = materialsByName.ContainsKey(matName) ? materialsByName[matName] : null;
+        Material mat = new Material(defaultMaterial);
+        mat.color = c2mat != null ? new Color32(c2mat.diffuseColor[0], c2mat.diffuseColor[1], c2mat.diffuseColor[2], c2mat.diffuseColor[3]) : Color.white;
+
+        if (c2mat != null && c2mat.flags.HasFlag(MatFileMaterial.Settings.Two_Sided))
+        {
+            mat.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+        }
+
+        if (mat.color.a < 1f)
+        {
+            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+        }
+
+        string texture = c2mat != null ? c2mat.texture : null;
+        mat.SetTexture("_MainTex", pixiesFile.GetFile(texture)?.bitmap);
+
+        materialCache[matName] = mat;
+        return mat;
+    }
+
     void Update()
     {
-
     }
 }
